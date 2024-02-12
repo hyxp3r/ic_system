51ceff0cd116b622c7e2122948095e070ef08180
Evgeniy <erma2001@mail.ru>
2024-02-12 17:49:34 +0700

    Добавлен Secrec key

--- api/config.py
+++ api/config.py
@@ -1,3 +1,20 @@
 from pathlib import Path
 
+from pydantic_settings import BaseSettings
+
 FILE_PATH = Path('C://', 'Users', 'e.n.ermakov', 'Desktop', 'file_ic', 'ЭК_ДЗ.xlsx')
+
+class ApiKeySettings(BaseSettings):
+    secret_key: str
+
+    class Config:
+        env_prefix = 'AUTH_'
+        env_file = '.env'
+
+
+class RedisSettings(BaseSettings):
+    url: str
+
+    class Config:
+        env_prefix = 'REDIS_'
+        env_file = '.env'
