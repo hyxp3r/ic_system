58e70f5a78b0f463c75c3dc2bda63cd0a6ca03ed
Evgeniy <erma2001@mail.ru>
2024-02-12 17:52:31 +0700

    Добавлены схемы для студента

---
+++ api/schemas/studentDTO.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from pydantic import BaseModel, field_validator
+
+
+class StudentSchemaAdd(BaseModel):
+    fio: str
+    personal_number: str
+    group: str
+    program: str
+    form: str
+    email: str|None  = None
+
+    @field_validator('personal_number')
+    def must_be_int(cls, v: str) -> str | ValueError:
+        try:
+            int(v)
+        except ValueError:
+            raise ValueError('mast contains only digits')
+        return v
+
+
+class StudentSchema(StudentSchemaAdd):
+    id: int
+
+    class Config:
+        from_attributes = True
