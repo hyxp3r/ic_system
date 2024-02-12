e5832b402303c9e848a7dedf290b7efea73dd600
Evgeniy <erma2001@mail.ru>
2024-02-12 11:26:47 +0700

    добавлена точка входа в api

---
+++ api/run_api.py
@@ -0,0 +1,15 @@
+from fastapi import  FastAPI
+
+from api.src.routers import all_routers
+
+
+app = FastAPI(
+    title = "ic_system",
+    root_path="/api/v1"
+
+)
+
+
+for router in all_routers:
+    app.include_router(router)
+
