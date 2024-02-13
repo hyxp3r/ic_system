cde6d540b60b5a5b8cf297915092bfe8f9a0a066
Evgeniy <erma2001@mail.ru>
2024-02-13 17:34:47 +0700

    поправлена структура кода

--- api/run_api.py
+++ api/run_api.py
@@ -1,15 +1,9 @@
-from fastapi import  FastAPI
+from fastapi import FastAPI
 
 from api.src.routers import all_routers
 
-
-app = FastAPI(
-    title = "ic_system",
-    root_path="/api/v1"
-
-)
+app = FastAPI(title='ic_system', root_path='/api/v1')
 
 
 for router in all_routers:
     app.include_router(router)
-
