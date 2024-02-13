9d7129094fb355cb5c7d18ea1d996b6ec0bb8760
Evgeniy <erma2001@mail.ru>
2024-02-13 17:34:30 +0700

    изменен класс redis

--- api/redis/redis.py
+++ api/redis/redis.py
@@ -1,21 +1,32 @@
 import os
-from redis import asyncio as aioredis
-from api.config import RedisSettings
 
+from redis import asyncio as aioredis
+
+from api.config import RedisSettings
 
 settings = RedisSettings()
 
 
 class Redis:
     """
-    config для подключения Redis 
+    config для подключения Redis
     """
-    def __init__(self):
-       
-        self.REDIS_URL = settings.url
-        self.connection_url = f"redis://{self.REDIS_URL}"
 
-    async def create_connection(self):
-        self.connection = aioredis.from_url(
-            self.connection_url, db=0)
-        return self.connection
+    def __init__(self):
+        self.REDIS_URL = settings.url
+        self.connection_url = f'redis://{self.REDIS_URL}'
+
+    async def __aenter__(self):
+        self.connection = aioredis.from_url(self.connection_url, db=0)
+
+    async def __aexit__(self, *args):
+        await self.connection.close()
+
+    async def get(self, key: str) -> str:
+        result = await self.connection.get(key)
+        if result:
+            return result.decode()
+
+    async def set(self, key: str, value: str, ex: int = None):
+        result = await self.connection.set(name=key, value=value, ex=ex)
+        return result
