447d2d4f8e3c069ba6e4ed0da1a2cc5165bc17ae
Evgeniy <erma2001@mail.ru>
2024-02-13 17:29:40 +0700

    поправлена структура кода

--- api/src/dependencies/tokens.py
+++ api/src/dependencies/tokens.py
@@ -1,32 +1,30 @@
-
-from fastapi import  HTTPException
-from fastapi import Header
+from fastapi import Header, HTTPException
 from jose import JWTError, jwt
 
 from api.config import ApiKeySettings
-from api.src.dependencies.uow import UOWDep
+from api.schemas.studentDTO import StudentSchema
 from api.services.student import StudentService
+from api.src.dependencies.uow import UOWDep
 
 settings = ApiKeySettings()
 
 SECRET_KEY = settings.secret_key
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ALGORITHM = settings.algorithm
 
-async def get_current_user(uow: UOWDep, token: str = Header(convert_underscores=False)):
+
+async def get_current_user(uow: UOWDep, token: str = Header(convert_underscores=False)) -> StudentSchema:
     credentials_exception = HTTPException(
         status_code=401,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
     )
     try:
-        token = token.replace("Bearer ", "")
+        token = token.replace('Bearer ', '')
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        personal_number: str = payload.get("sub")
+        personal_number: str = payload.get('sub')
         if personal_number is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
     user = await StudentService().get_student_by_personal_number(uow, personal_number)
     return user
-
--- api/src/dependencies/uow.py
+++ api/src/dependencies/uow.py
@@ -1,7 +1,7 @@
 from typing import Annotated
 
 from fastapi import Depends
-from api.utils.unitofwork import IUnitOfWork, UnitOfWork
 
+from api.utils.unitofwork import IUnitOfWork, UnitOfWork
 
 UOWDep = Annotated[IUnitOfWork, Depends(UnitOfWork)]
