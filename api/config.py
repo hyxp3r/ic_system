31f5a6aba157b30eb4d7d5bc1fa4ac517f377b43
Evgeniy <erma2001@mail.ru>
2024-02-13 17:35:26 +0700

    изменен конфик для token, добавлен конфиг для рассылки

--- api/config.py
+++ api/config.py
@@ -4,8 +4,10 @@ from pydantic_settings import BaseSettings
 
 FILE_PATH = Path('C://', 'Users', 'e.n.ermakov', 'Desktop', 'file_ic', 'ЭК_ДЗ.xlsx')
 
+
 class ApiKeySettings(BaseSettings):
     secret_key: str
+    algorithm: str
 
     class Config:
         env_prefix = 'AUTH_'
@@ -17,4 +19,16 @@ class RedisSettings(BaseSettings):
 
     class Config:
         env_prefix = 'REDIS_'
+        env_file = '.env'
+
+
+class MailSettings(BaseSettings):
+    from_email: str
+    smtp_server: str
+    smtp_port: int
+    smtp_username: str
+    smtp_password: str
+
+    class Config:
+        env_prefix = 'MAIL_'
         env_file = '.env'
