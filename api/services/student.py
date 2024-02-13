3faa20fb15ffaa41c85104352c95828926292ec3
Evgeniy <erma2001@mail.ru>
2024-02-13 17:31:26 +0700

    Дописан сервис по авторизации студента

--- api/services/student.py
+++ api/services/student.py
@@ -2,18 +2,21 @@ import random
 import string
 
 from fastapi import HTTPException, status
-from jose import JWTError, jwt
+from jose import jwt
 
-from api.utils.unitofwork import IUnitOfWork
-from api.redis.redis import Redis
 from api.config import ApiKeySettings
+from api.redis.redis import Redis
+from api.schemas.authDTO import Token
+from api.tasks.tasks import send_verification_code_task
+from api.utils.unitofwork import IUnitOfWork
 
 settings = ApiKeySettings()
 
 
 SECRET_KEY = settings.secret_key
-ALGORITHM = "HS256"
+ALGORITHM = 'HS256'
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 class StudentService:
 
@@ -21,40 +24,33 @@ class StudentService:
         async with uow:
             student = await uow.students.get_student_by_personal_number(personal_number=personal_number)
         return student
-    
 
     async def make_verify_code(self, uow: IUnitOfWork, personal_number: str):
+        redis = Redis()
         async with uow:
             student = await uow.students.get_student_by_personal_number(personal_number=personal_number)
         personal_number = student.personal_number
         email = student.email
-        print(email)
+        if not email:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='Email is required for sending code'
+            )
+        verification_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+        async with redis:
+            set_result = await redis.set(key=personal_number, value=verification_code, ex=300)
+        send_verification_code_task.delay(email, verification_code)
 
-        verification_code = "".join(random.choices(string.ascii_uppercase + string.digits, k=6))
-
+    async def verify_code(self, uow: IUnitOfWork, personal_number: str, verification_code: str) -> Token:
         redis = Redis()
-        redis_conn = await redis.create_connection()
-        await redis_conn.set(name=personal_number, value=verification_code, ex=300)
-        await redis_conn.close()
-        return verification_code
-    
-    async def verify_code(self, uow: IUnitOfWork, personal_number:str, verification_code:str):
-        
-        redis = Redis()
-        redis_conn = await redis.create_connection()
-        redis_code = await redis_conn.get(personal_number)
-        await redis_conn.close()
-        if redis_code:
-            redis_code = redis_code.decode()
-        else:
+        async with redis:
+            redis_code = await redis.get(personal_number)
+        if not redis_code:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Code was't found")
-
-        if redis_code == verification_code:
-            token_data = {"sub": personal_number}
-            token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
-            async with uow:
-                await uow.students.update_student_api_key(personal_number=personal_number, api_key=token)
-                await uow.commit()
-
-            return {"access_token": token, "token_type": "bearer"}  
-        
+        if redis_code != verification_code:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid verification code')
+        token_data = {'sub': personal_number}
+        token = jwt.encode(token_data, SECRET_KEY, algorithm=ALGORITHM)
+        async with uow:
+            await uow.students.update_student_api_key(personal_number=personal_number, api_key=token)
+            await uow.commit()
+        return Token(access_token=token, token_type='bearer')
