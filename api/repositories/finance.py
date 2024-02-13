3c79d419cb220242a1b991828ab0c3739045e220
Evgeniy <erma2001@mail.ru>
2024-02-13 17:32:52 +0700

    поправлена структура кода

--- api/repositories/finance.py
+++ api/repositories/finance.py
@@ -1,22 +1,21 @@
-
 from typing import Type
+
 from fastapi import HTTPException, status
 from sqlalchemy import select
 
+from api.schemas.financeDTO import FinanceSchema
 from api.utils.repository import SQLAlchemyRepository
 from db import FinancialIndebtedness
-from api.schemas.financeDTO import FinanceSchema
 
 
 class FinanceRepository(SQLAlchemyRepository):
-    
+
     model = FinancialIndebtedness
 
-    async def get_finance_by_personal_number(self, personal_number:str) -> Type[FinanceSchema]:
-        stmt = select(self.model).filter_by(personal_number = personal_number, status = True)
+    async def get_finance_by_personal_number(self, personal_number: str) -> Type[FinanceSchema]:
+        stmt = select(self.model).filter_by(personal_number=personal_number, status=True)
         res = await self.session.execute(stmt)
         res = res.one_or_none()
         if not res:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Record not found')
         return res[0].to_read_model()
-
--- api/repositories/students.py
+++ api/repositories/students.py
@@ -1,28 +1,26 @@
 from typing import Type
+
 from fastapi import HTTPException, status
 from sqlalchemy import select, update
 
+from api.schemas.studentDTO import StudentSchema
 from api.utils.repository import SQLAlchemyRepository
 from db import Students
-from api.schemas.studentDTO import StudentSchema
 
 
 class StudentRepository(SQLAlchemyRepository):
-    
+
     model = Students
 
-    async def get_student_by_personal_number(self, personal_number:str) -> Type[StudentSchema]:
-        stmt = select(self.model).filter_by(personal_number = personal_number)
+    async def get_student_by_personal_number(self, personal_number: str) -> Type[StudentSchema]:
+        stmt = select(self.model).filter_by(personal_number=personal_number)
         res = await self.session.execute(stmt)
         res = res.one_or_none()
         if not res:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Record not found')
         student = res[0].to_read_model()
         return student
-    
 
-    async def update_student_api_key(self, personal_number:str, api_key:str) -> Type[StudentSchema]:
-        stmt = update(self.model).filter_by(personal_number = personal_number).values(api_key = api_key)
+    async def update_student_api_key(self, personal_number: str, api_key: str) -> Type[StudentSchema]:
+        stmt = update(self.model).filter_by(personal_number=personal_number).values(api_key=api_key)
         res = await self.session.execute(stmt)
-        
-
