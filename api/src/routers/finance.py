a87ee3c039b7f96ab4dc8ebdc6d7dba047971b0b
Evgeniy <erma2001@mail.ru>
2024-02-13 17:30:19 +0700

    поправлена структура кода и добавлены роутеры на авторизацию

--- api/src/routers/finance.py
+++ api/src/routers/finance.py
@@ -1,28 +1,31 @@
 from fastapi import APIRouter, Depends, status
 
-from api.src.dependencies.uow import UOWDep
-from api.src.dependencies.tokens import get_current_user
 from api.schemas.financeDTO import FinanceSchema
 from api.services.finance import FinanceService
+from api.src.dependencies.tokens import get_current_user
+from api.src.dependencies.uow import UOWDep
 
 router = APIRouter(
-    prefix="/finances",
-    tags=["Finance"],
+    prefix='/finances',
+    tags=['Finance'],
 )
 
-@router.get("",
-            response_model=FinanceSchema,
-            status_code=status.HTTP_200_OK,
-            responses={
-                status.HTTP_200_OK: {
-                    "model": FinanceSchema, 
-                    "description": "Ok Response",
-                                     }                   
-                      }
-            )
-async def get_finance(uow: UOWDep,
-                      user =  Depends(get_current_user),
-                       ):
+
+@router.get(
+    '',
+    response_model=FinanceSchema,
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'model': FinanceSchema,
+            'description': 'Ok Response',
+        }
+    },
+)
+async def get_finance(
+    uow: UOWDep,
+    user=Depends(get_current_user),
+):
     personal_number = user.personal_number
     finances = await FinanceService().get_finance_by_personal_number(uow, personal_number=personal_number)
     return finances
--- api/src/routers/students.py
+++ api/src/routers/students.py
@@ -1,27 +1,38 @@
 from fastapi import APIRouter, status
 
-from api.src.dependencies.uow import UOWDep
+from api.schemas.authDTO import Token
 from api.services.student import StudentService
+from api.src.dependencies.uow import UOWDep
 
 router = APIRouter(
-    prefix="/students",
-    tags=["Students"],
+    prefix='/students',
+    tags=['Students'],
 )
 
-@router.get("/creating_codes",
-            status_code=status.HTTP_200_OK,)
-async def make_code(uow: UOWDep,
-                       personal_number:str):
-    
-    verify_code = await StudentService().make_verify_code(uow, personal_number=personal_number)
-    return {"status":"Ok", "verify_code":verify_code}
+
+@router.get(
+    '/creating_codes',
+    status_code=status.HTTP_200_OK,
+)
+async def make_code(uow: UOWDep, personal_number: str):
+
+    await StudentService().make_verify_code(uow, personal_number=personal_number)
+    return {'status': 'Ok', 'detail': 'Code was sended'}
 
 
-@router.get("/verifying_codes",
-            status_code=status.HTTP_200_OK,)
-async def verify_code(uow: UOWDep,
-                       personal_number:str,
-                       verification_code:str):
-    
-    api_token = await StudentService().verify_code(uow, personal_number=personal_number, verification_code=verification_code)
+@router.get(
+    '/verifying_codes',
+    response_model=Token,
+    status_code=status.HTTP_200_OK,
+    responses={
+        status.HTTP_200_OK: {
+            'model': Token,
+            'description': 'Ok Response',
+        }
+    },
+)
+async def verify_code(uow: UOWDep, personal_number: str, verification_code: str):
+    api_token = await StudentService().verify_code(
+        uow, personal_number=personal_number, verification_code=verification_code
+    )
     return api_token
