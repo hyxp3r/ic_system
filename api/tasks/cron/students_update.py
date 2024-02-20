2755d270402289c2786596a23bcd1b3ffcd9626a
Evgeniy <erma2001@mail.ru>
2024-02-20 17:47:40 +0700

    Add all functions for task studend_update

--- api/tasks/cron/students_update.py
+++ api/tasks/cron/students_update.py
@@ -1,15 +1,45 @@
-from sqlalchemy import select
+from sqlalchemy import insert, select, update
 
+from api.schemas.studentDTO import StudentSchema, StudentSchemaAdd
+from api.utils.tandem import Tandem
 from db import Students, sync_session
 
-
-
-
-
-def get_students_with_key():
+def get_students_with_key() -> list[StudentSchema]:
     stmt = select(Students).filter(Students.api_key.isnot(None))
     with sync_session() as session:
         res = session.execute(stmt)
         students_api =  [row[0].to_read_model() for row in res.all()]
-    print(students_api)
     return students_api
+
+def get_students_tandem() -> list[StudentSchemaAdd]:
+    tandem_session = Tandem()
+    with tandem_session:
+        students_tandem = tandem_session.get_students()
+    students_tandem_schemas = []
+    for student in students_tandem:
+        student_schema = StudentSchemaAdd(
+            fio=student["fio"],
+            personal_number=student["personal_number"],
+            group=student["group"],
+            program=student["program"],
+            form = student["form"],
+            email = student["email"],
+        )
+        students_tandem_schemas.append(student_schema)
+    return students_tandem_schemas
+
+def update_api_keys(sudents_api:list[StudentSchema]):
+    if len(sudents_api) != 0:
+        for student in sudents_api:
+            stmt = update(Students).filter_by(personal_number=student.personal_number).values(api_key=student.api_key)
+            with sync_session() as session:
+                session.execute(stmt)
+                session.commit()
+
+def insert_students(students_tandem_schemas:list[StudentSchemaAdd]):
+
+        with sync_session() as session:
+                for student in students_tandem_schemas:
+                    stmt = insert(Students).values(**student.model_dump())
+                    session.execute(stmt)
+                session.commit()
