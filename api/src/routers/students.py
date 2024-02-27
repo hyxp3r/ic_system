ec817724d834e29d5c422346e6346915f31a7473
Evgeniy <erma2001@mail.ru>
2024-02-27 17:08:10 +0700

    update specification

--- api/src/routers/students.py
+++ api/src/routers/students.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status
 
-from api.schemas.authDTO import Token
+from api.schemas.studentDTO import StudentSchema
 from api.services.student import StudentService
 from api.src.dependencies.uow import UOWDep
 
@@ -15,7 +15,6 @@ router = APIRouter(
     status_code=status.HTTP_200_OK,
     responses= {
         status.HTTP_200_OK: {
-            'model': Token,
             'description': 'Ok Response',
         },
         status.HTTP_404_NOT_FOUND: {
@@ -27,17 +26,17 @@ router = APIRouter(
     }
 )
 async def create_code(uow: UOWDep, personal_number: str):
-    await StudentService().make_verify_code(uow, personal_number=personal_number)
-    return {'status': 'Ok', 'detail': 'Code was sended'}
+    email = await StudentService().make_verify_code(uow, personal_number=personal_number)
+    return {'status': 'Ok', 'email':email}
 
 
 @router.get(
     '/verifying_codes',
-    response_model=Token,
+    response_model=StudentSchema,
     status_code=status.HTTP_200_OK,
     responses={
         status.HTTP_200_OK: {
-            'model': Token,
+            'model': StudentSchema,
             'description': 'Ok Response',
         },
     },
@@ -46,4 +45,5 @@ async def verify_code(uow: UOWDep, personal_number: str, verification_code: str)
     api_token = await StudentService().verify_code(
         uow, personal_number=personal_number, verification_code=verification_code
     )
-    return api_token
+    user = await StudentService().get_student_by_personal_number(uow, personal_number)
+    return user
