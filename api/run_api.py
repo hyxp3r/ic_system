0ef371f4b8c7db5dd665cc92665e72d2407c2286
Evgeniy <erma2001@mail.ru>
2024-02-28 18:12:21 +0700

    update specification

--- api/run_api.py
+++ api/run_api.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 from api.src.routers import all_routers
 from api.tasks import update_finance_table, update_students_table
+from api.utils.tandem import TandemSettings
+
+settings = TandemSettings()
+
 app = FastAPI(title='ic_system', root_path='/api/v1')
 
 
@@ -12,6 +16,6 @@ for router in all_routers:
 
 @app.on_event("startup")
 def startup_db_client():
-   
+    print(settings.model_dump())
     #update_finance_table()
     update_students_table()
